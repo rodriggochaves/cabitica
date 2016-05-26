cc058d523c6d11b17bfdacc2092755bd6da13fcc
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-05-26 17:20:46 -0300

    Added javascript for upvote and downvote, re-structured

--- app/assets/javascripts/habits.js
+++ app/assets/javascripts/habits.js
@@ -1,24 +1,35 @@
-var completeHabit = function(switchHabitElem) {    
-  var habitCallout = $(switchHabitElem).closest('.habit__callout');
-  var id = habitCallout.attr("id");
+var upvoteHabit = function(upvoteHabitElem) {    
+  var id = $(upvoteHabitElem).closest('.habit__callout').attr("id");
   var habitId = extractIdNumber(id);
-  var sendCompleteHabitRequest = function() {
-    $.ajax({
-      url: '/complete_habit/' + habitId,
-      type: 'GET',
-      success: function(resp) {
-        console.log(resp)
-      },
-      error: function(err) {
-        console.log(err)
-      }
-    });
-  };
-  if(switchHabitElem.checked){
-    $(habitCallout).fadeOut(3000, "swing", sendCompleteHabitRequest);
-  } else {
-    $(habitCallout).stop().animate({opacity:'100'});
-  }
+  console.log("Sent upvote for habit " + habitId);
+  // $.ajax({
+  //   url: '/upvote_habit/' + habitId,
+  //   type: 'GET',
+  //   success: function(resp) {
+  //     console.log("upvoted habit " + habitId);
+  //   },
+  //   error: function(err) {
+  //     console.log("Error upvoting habit: ");
+  //     console.log(err);
+  //   }
+  // });
+}
+
+var downvoteHabit = function(downvoteHabitElem) {
+  var id = $(downvoteHabitElem).closest('.habit__callout').attr("id");
+  var habitId = extractIdNumber(id);
+  console.log("Sent downvote for habit " + habitId);
+  // $.ajax({
+  //   url: '/downvote_habit/' + habitId,
+  //   type: 'GET',
+  //   success: function(resp) {
+  //     console.log("downvote habit " + habitId);
+  //   },
+  //   error: function(err) {
+  //     console.log("Error downvoting habit: ");
+  //     console.log(err);
+  //   }
+  // });
 }
 
 var removeHabit = function(removeHabitElem){
@@ -55,10 +66,12 @@ $(document).ready(function() {
       }
     });
   });
-  $('.habits__list').on("click", ".switch-input", function(){
-    completeHabit(this);
-  });
-  $('.habits__list').on("click", ".close-button", function(){
-    removeHabit(this);
-  });
+  $('.habits__list')
+    .on("click", ".habit__plus", function(){
+      upvoteHabit(this);
+    }).on("click", ".habit__minus", function(){
+      downvoteHabit(this);
+    }).on("click", ".close-button", function(){
+      removeHabit(this);
+    });
 });
