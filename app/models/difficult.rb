0f25086ec2bf216354b18f9926dfa90fad2df0c1
rodriggochaves <rodriggo.chaves@gmail.com>
2016-09-05 08:22:47 -0300

    teste de relacionamento

--- app/models/difficult.rb
+++ app/models/difficult.rb
@@ -2,5 +2,5 @@ class Difficult < ActiveRecord::Base
   has_many :tasks, class_name: "Task",
                     foreign_key: "difficult_id"
   has_many :habits, class_name: "Habit",
-                    foreign_key: "habit_id"
+                    foreign_key: "difficult_id"
 end
